42856c7f238321207779a9851c3d38f849ba0ebc
bdd_model_car_admin <sauhaarda@gmail.com>
2017-08-16 00:56:56 -0700

    Fixed major bugx

--- install_users.sh
+++ install_users.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-for dir in */ ; do
-USER=${dir::-1}
+USER=$1
 useradd -m $USER
 su $USER <<'EOF'
 mkdir -p ~/.ssh
 chmod 0700 ~/.ssh
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -b 4096
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -b 4096 > /dev/null
 EOF
 cp $USER/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys
 chown $USER:$USER /home/$USER/.ssh/authorized_keys
 cd $USER
 cp -R * .[^.]* /home/$USER/
 chsh -s `which bash` $USER
-done
+sleep 2
 
 # DEFAULT PASSWORDS AND SUDO USERS
 usermod -aG sudo sauhaarda
