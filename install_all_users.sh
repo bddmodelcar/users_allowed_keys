5b7df0160899bfa50f009eaea0c9e46dcea8c4e5
sauhaarda <sauhaarda@gmail.com>
2017-10-11 16:58:56 -0700

    Added sudo users to the repo

--- install_all_users.sh
+++ install_all_users.sh
@@ -4,3 +4,23 @@ USER=${dir::-1}
 ./install_users.sh $USER
 usermod -aG docker $USER
 done
+
+# Always sudo users
+usermod -aG sudo sauhaarda
+usermod -aG sudo sascha
+usermod -aG sudo karlzipser
+
+host=`hostname`
+if [ $host == 'bdd3' ]; then # bdd3 sudo users
+    usermod -aG sudo bala
+fi
+if [ $host == 'bdd4' ]; then # bdd4 sudo users
+    usermod -aG sudo aga
+fi
+if [ $host == 'bdd5' ]; then # bdd4 sudo users
+    usermod -aG sudo ehou
+fi
+if [ $host == 'bdd6' ]; then # bdd4 sudo users
+    usermod -aG sudo danqing0703
+    usermod -aG sudo yexia
+fi
